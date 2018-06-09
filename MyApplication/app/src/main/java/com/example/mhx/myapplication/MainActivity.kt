d38044e694d4a6790c6cbb936709056e5492aef9
MHX <595455613qq.com>
2018-06-09 22:37:54 +0800

    注释语句

---
+++ MyApplication/app/src/main/java/com/example/mhx/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mhx.myapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ MyApplication2/app/src/main/java/com/example/mhx/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mhx.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+ private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.login_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"SUCESS!",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
